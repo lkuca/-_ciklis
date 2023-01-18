1b40acd30abef166d5ba44b365f804814402ea58
lkuca <120181604+lkuca@users.noreply.github.com>
2023-01-18 16:47:14 +0200

    Update Контрольнаяработа_cikli.py

--- Контрольнаяработа_cikli.py
+++ Контрольнаяработа_cikli.py
@@ -1,14 +1,23 @@
 from random import *
-from statistics import mean
-s=int(input("viberite skolko lis hotite"))
-for n in range(s):
-    if s<=10:
-        print(" ^---^")
-        print("( o o )")
-        print(" ! = !/)")
-        print("")
-        print("")
-        print("")
+from math import *
+try:
+    n=int(input("vali 1-9 numbrist et panda nii palju rebasi"))
+    if n<1 or n>9:
+        print("vale")
+except:
+      print("vale")
+x=0
+for x in range(4):
+    for e in range(n):
+        if x==0:
+            print(" ^---^ ",end="")
+        elif x== 1:
+            print("( o o )",end="")
+        elif x==2:
+            print("! = !/)",end="")
+    print("")
+
+
 nch=int(input("sissestage arv"))
 st=int(input("sissestage aste"))
 nch1 = 1
@@ -18,8 +27,10 @@ for i in range(1,nch+1):
     print(nch1,"^",st,"=",stn)
     nch1 +=1
     st1 +=1
-bruh =randint(0,6)
+
+bruh =randint(1,5)
 õpilaste = randint(1,16)
-print(f"keskmine hinne õpilastel={bruh+õpilas"),
-format(mean(int(print(f'hinne:{bruh} '))))
-print(f' õpilase arv:{õpilaste} ')
+if bruh<2:
+    print(f"minimaalne hinne {bruh}")
+elif bruh>4:
+    print(f"maksimaalne hinne{bruh}")
